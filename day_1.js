3290b48dc5ad716134b6a210357269d83a8afe95
jseo <ahndjds@gmail.com>
2025-06-25 17:24:29 +0900

    첫 push: day_1.js, quiz17.html, quiz20.html, pxToEM.html, pxToEM.css, card_836_1.jpg

---
+++ day_1.js
@@ -0,0 +1,42 @@
+// quiz 1
+// let name = "ghost";
+// const age = 1000;
+// console.log(name, age);
+
+//quiz 2
+// let age = 25;
+// age = 30;
+// console.log(age);
+
+//quiz 6
+// const user = { name: "Bob" };
+// user.name = "Tom";
+// console.log(user);
+// user.age = 1000;
+// console.log(user);
+
+// quiz 7
+// const name = "ghost";
+// const age = 1000;
+// const user = `안녕하세요, 저는${name} 이고 ${age} 살입니다`;
+// console.log(user);
+
+// quiz 12
+// const num1 = 3;
+// const num2 = 5;
+// const result = `${num1} + ${num2} = ${num1 + num2}`;
+// console.log(result);
+
+// function sum(a, b) {
+//   const result = `${a} + ${b} = ${a + b}`;
+//   return result;
+// }
+// console.log(sum(1, 3));
+
+// quiz 13
+function calc(a, b) {
+  console.log(`${a} * ${b} = ${a * b}`);
+  console.log(`${a} / ${b} = ${a / b}`);
+}
+
+calc(6, 2);
