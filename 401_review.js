ef8131e9047b806fd292be6cc4624f440266e6c8
jseo <ahndjds@gmail.com>
2025-07-01 10:01:57 +0900

    401_review.js 업데이트

--- 401_review.js
+++ 401_review.js
@@ -1,1 +1,58 @@
+console.clear();
 
+//1.
+// const arr = [1, 2, 3, 4, 5, 6];
+// console.log(arr.filter((x) => !(x % 2)));
+//2.
+// const arr2 = [1, 2, 3, 4];
+// console.log(arr2.map((x) => x ** 2));
+//3.
+// const arr = [5, 2, 9, 1];
+// console.log(arr.sort((a, b) => b - a));
+//4.
+// const arr2 = [1, 2, 3, 4, 5];
+// console.log(arr2.reduce((acc, curr) => acc + curr, 0));
+//5.
+// const arr = [3, -1, 5, 7];
+// console.log(arr.some((x) => x < 0));
+//6.
+// const arr2 = [4, 5, 6];
+// console.log(arr2.every((x) => x > 3));
+//7.
+// function solution(names) {
+//   names.forEach((name) => {
+//     console.log(name);
+//   });
+// }
+// solution(["Irangi", "Camel", "Shoopa"]);
+//8.
+// function solution2(scores) {
+//   return scores.filter((score) => score >= 60).map((score) => score * 2);
+// }
+// console.log(solution2([50, 60, 70, 80]));
+//9.
+// function solution(my_string) {
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   return my_string
+//     .split("")
+//     .filter((char) => !vowels.includes(char))
+//     .join("");
+// }
+
+// console.log(solution("beautiful"));
+
+//10.
+// for (i = 1; i < 6; i++) {
+//   console.log(i);
+// }
+//11.
+// const arr = [];
+// function solution() {
+//   for (i = 1; i < 11; i++) {
+//     if (i % 3 === 0) {
+//       arr.push(i);
+//     }
+//   }
+//   return arr;
+// }
+// console.log(solution());
